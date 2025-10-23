78aa596a617e81dfb696ec923fdacd0fd220d1cf
Emiliano <EmilianoR117@outlook.com>
2025-10-22 22:52:44 -0600

    feat: error handling

---
+++ meli-app/src/main/java/org/digitalnao/meli/web/RestExceptionHandler.java
@@ -0,0 +1,30 @@
+package org.digitalnao.meli.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("message", "Validation failed");
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError fe : ex.getBindingResult().getFieldErrors()) {
+            errors.put(fe.getField(), fe.getDefaultMessage());
+        }
+        body.put("errors", errors);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
+    }
+}
