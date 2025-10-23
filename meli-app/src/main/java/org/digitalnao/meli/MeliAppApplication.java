761bbb8fccba39d7ae2b638e203e2b7197ba0c2d
Emiliano <EmilianoR117@outlook.com>
2025-10-22 22:45:43 -0600

    first commit

---
+++ meli-app/src/main/java/org/digitalnao/meli/MeliAppApplication.java
@@ -0,0 +1,13 @@
+package org.digitalnao.meli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class MeliAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MeliAppApplication.class, args);
+    }
+}
